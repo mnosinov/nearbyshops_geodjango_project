ee2cd24f501dd9aba227f80680784aeeabab4dc6
mnosinov <mnosinov@gmail.com>
2021-04-14 15:12:48 +0600

    first 7 closest shops list is ready

---
+++ shops/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ShopList
+
+urlpatterns = [
+    path('', ShopList.as_view()),
+]
--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+
+longitude = 43.252077
+latitude = 76.947657
+
+user_location = Point(longitude, latitude, srid=4326)
+
+
+class ShopList(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(
+        distance=Distance('location', user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'shops/index.html'
